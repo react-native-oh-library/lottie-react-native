4626175157691dc7ff47057eeb8989a868648c64
“leehom6666” <liulihong6@h-partners.com>
2024-11-28 11:34:58 +0800

    feat: codegen-lib适配
    Signed-off-by: “leehom6666” <liulihong6@h-partners.com>

--- harmony/lottie/src/main/cpp/LottieAnimationViewPackage.h
+++ harmony/lottie/src/main/cpp/LottieAnimationViewPackage.h
@@ -1,53 +1,13 @@
-/*
- * Copyright (C) 2023 Huawei Device Co., Ltd.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * you may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+// Use of this source code is governed by a Apache-2.0 license that can be
+// found in the LICENSE file.
 
-#ifndef LOTTIE_SRC_MAIN_CPP_LOTTIEANIMATIONVIEWPACKAGE_H
-#define LOTTIE_SRC_MAIN_CPP_LOTTIEANIMATIONVIEWPACKAGE_H
+#pragma once
 
-#include "RNOH/Package.h"
-#include "ComponentDescriptor.h"
-#include "LottieAnimationViewNapiBinder.h"
-#include "LottieAnimationViewJSIBinder.h"
-#include "LottieAnimationViewEventRequestHandler.h"
-
-using namespace rnoh;
-using namespace facebook;
-
+#include "generated/RNOH/generated/BaseLottieReactNativePackage.h"
 namespace rnoh {
-
-    class LottieAnimationViewPackage : public Package {
-    public:
-        explicit LottieAnimationViewPackage(Package::Context ctx) : Package(ctx) {}
-
-        std::vector<facebook::react::ComponentDescriptorProvider> createComponentDescriptorProviders() override {
-            return {react::concreteComponentDescriptorProvider<react::LottieAnimationViewComponentDescriptor>()};
-        }
-
-        ComponentNapiBinderByString createComponentNapiBinderByName() override {
-            return {{"LottieAnimationView", std::make_shared<LottieAnimationViewNapiBinder>()}};
-        }
-
-        ComponentJSIBinderByString createComponentJSIBinderByName() override {
-            return {{"LottieAnimationView", std::make_shared<LottieAnimationViewJSIBinder>()}};
-        };
-
-        EventEmitRequestHandlers createEventEmitRequestHandlers() override {
-            return {std::make_shared<LottieAnimationViewEventRequestHandler>()};
-        }
-    };
+class LottieAnimationViewPackage : public BaseLottieReactNativePackage {
+    using Super = BaseLottieReactNativePackage;
+    using Super::Super;
+};
 } // namespace rnoh
-
-#endif
--- harmony/lottie/src/main/ets/common/Animation.ts
+++ harmony/lottie/src/main/ets/common/Animation.ts
@@ -1,3 +1,7 @@
+// Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+// Use of this source code is governed by a Apache-2.0 license that can be
+// found in the LICENSE file.
+
 export interface layersItem {
   "ddd"?: number;
   "ind"?: number;
